70e00bfa33c9f13ed3f167c0ce59e017ece2d141
Yousef brighith <128628811+brighith@users.noreply.github.com>
2023-07-07 16:26:09 +0300

    rotate

--- ArraysToDo2.js
+++ ArraysToDo2.js
@@ -10,3 +10,13 @@ function reverse(arr) {
   return arr;
 }
 console.log(reverse([1, 4, 6, 7, 8, 9, 0, 7]));
+
+// Rotate
+function rotateArr(arr, shiftBy) {
+  for (var i = 0; i < shiftBy; i++) {
+    current = arr.pop();
+    arr.unshift(current);
+  }
+  return arr;
+}
+console.log(rotateArr([1, 2, 3, 4, 5, 6, 7], 1));
